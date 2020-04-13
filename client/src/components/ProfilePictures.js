203da9d92bf4d8c1c8bbf4070608f5de06511cfb
nadinho <rockt2935@gmail.com>
2020-04-13 11:18:11 +0200

    Adjust use of properties for profile pictures

--- client/src/components/ProfilePictures.js
+++ client/src/components/ProfilePictures.js
@@ -3,13 +3,12 @@ import styled from "@emotion/styled";
 const ProfilePicture = styled.img`
   border-radius: 100px;
   height: ${(props) => profilePictureSizes[props.size].height};
-  width: ${(props) => profilePictureSizes[props.size].width};
 `;
 
 const profilePictureSizes = {
-  small: { height: "16px", width: "16px" },
-  medium: { height: "40px", width: "40px" },
-  big: { height: "60px", width: "60px" },
+  small: { height: "16px" },
+  medium: { height: "40px" },
+  big: { height: "60px" },
 };
 
 export default ProfilePicture;
