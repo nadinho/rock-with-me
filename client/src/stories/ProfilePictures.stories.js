1b9cc0e893a140a2cff83d9aa861d9ca38ccf857
nadinho <rockt2935@gmail.com>
2020-04-13 15:16:29 +0200

    Remove modul in stories

--- client/src/stories/ProfilePictures.stories.js
+++ client/src/stories/ProfilePictures.stories.js
@@ -1,10 +1,8 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import centered from "@storybook/addon-centered/react";
 import ProfilePictures from "../components/ProfilePictures";
 
 storiesOf("ProfilePictures", module)
-  .addDecorator(centered)
   .add("small", () => (
     <ProfilePictures
       src="https://randomuser.me/api/portraits/women/57.jpg"
