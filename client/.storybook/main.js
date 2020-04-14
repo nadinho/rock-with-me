38ff8732710c490bb570034cd5908b2d9b2f0fc8
Nadine Gehrmann <57504815+nadinho@users.noreply.github.com>
2020-04-14 09:51:02 +0200

    Deployment fix (#22)
    * Revert "Remove storybook addon"
    
    This reverts commit ab9ba2d9767613034055e26c0e0b4745980960b9.
    
    * Remove storybook addon
    
    * Remove exported module
    
    * Fix doctype issue
    
    * Fix naming issue
    
    * Remove modul in stories
    
    * Remove storybook addon
    
    * Remove problematic preset

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
   stories: ["../src/**/*.stories.js"],
   addons: [
-    "@storybook/preset-create-react-app",
     "@storybook/addon-actions",
     "@storybook/addon-links",
     "@storybook/addon-knobs/register",
