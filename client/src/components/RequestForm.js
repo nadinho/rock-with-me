09596b578459fbca468ffe09415cccea663eceab
nadinho <rockt2935@gmail.com>
2020-04-16 13:43:47 +0200

    Outsource Input

--- client/src/components/RequestForm.js
+++ client/src/components/RequestForm.js
@@ -1,8 +1,9 @@
 import styled from "@emotion/styled";
 import React from "react";
 import colors from "../utils/colors";
+import { Input } from "./Input";
 
-export default function ConcertCardBig() {
+export default function RequestForm() {
   return (
     <Form type="submit">
       <Div>
@@ -51,33 +52,4 @@ const Form = styled.form`
   flex-direction: column;
 
   color: ${colors.primaryText};
-`;
-
-const Input = styled.input`
-  margin: 10px;
-
-  border-right: 2px solid transparent;
-  border-image: linear-gradient(
-    50deg,
-    ${colors.gradientOne},
-    ${colors.gradientTwo}
-  );
-  border-image-slice: 1;
-  font-family: "Montserrat", sans-serif;
-  background: transparent;
-  background-size: 10px 100%;
-  background-position: 0 0, 100% 0;
-  background-repeat: no-repeat;
-  outline: none;
-
-  font-size: 1rem;
-  color: ${colors.primaryText};
-  padding: 10px;
-
-  caret-color: red;
-
-  ::placeholder {
-    color: ${colors.primaryText};
-    opacity: 0.6;
-  }
 `;
