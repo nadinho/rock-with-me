41678dc98348792462f0d4f5324217b83a6aa546
nadinho <rockt2935@gmail.com>
2020-04-17 16:38:59 +0200

    Create page routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
 import React from "react";
 import GlobalStyle from "./GlobalStyle";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Start from "./pages/Start";
+import Home from "./pages/Home";
+import Konzerte from "./pages/Konzerte";
+import Messages from "./pages/Messages";
+import Profil from "./pages/Profil";
 
 export default function App() {
   return (
-    <>
-      <GlobalStyle></GlobalStyle>
-      Guten Tach
-    </>
+    <Router>
+      <GlobalStyle />
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Start />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/konzerte">
+            <Konzerte />
+          </Route>
+          <Route path="/messages">
+            <Messages />
+          </Route>
+          <Route path="/profil">
+            <Profil />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
