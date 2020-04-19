a0107a087ea8c51b42c907c30152043e08db3a67
nadinho <rockt2935@gmail.com>
2020-04-19 14:42:00 +0200

    Outsource animations

--- client/src/components/ButtonContainer.js
+++ client/src/components/ButtonContainer.js
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { fadeIn } from "../utils/animations";
 
 export const ButtonContainer = styled.div`
   display: flex;
@@ -6,15 +7,5 @@ export const ButtonContainer = styled.div`
   align-items: center;
   margin-top: 20px;
   animation-delay: 200ms;
-  animation: fadeIn 1500ms ease-in both;
-  @keyframes fadeIn {
-    from {
-      opacity: 0;
-      transform: translate3d(0, -20%, 0);
-    }
-    to {
-      opacity: 1;
-      transform: translate3d(0, 0, 0);
-    }
-  }
+  animation: ${fadeIn} 1500ms ease-in;
 `;
