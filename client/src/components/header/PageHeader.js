60854c09b4ff4e14648658c6374ed384c7e1af2a
nadinho <rockt2935@gmail.com>
2020-04-21 18:44:18 +0200

    Fix paths

--- client/src/components/header/PageHeader.js
+++ client/src/components/header/PageHeader.js
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 import React from "react";
-import Logo from "../assets/icons/platzhalter-logo.png";
-import BuddyRequest from "../assets/icons/add-icon.png";
+import Logo from "../../assets/icons/platzhalter-logo.png";
+import BuddyRequest from "../../assets/icons/add-icon.png";
 
 export default function PageHeader(props) {
   return (
