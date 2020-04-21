939b4988a9765e107346cd9f7f8c01f00d0a6dde
nadinho <rockt2935@gmail.com>
2020-04-21 10:41:19 +0200

    Create hook for setting the concert

---
+++ client/src/hooks/useGetConcert.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { getConcert } from "../api/Concerts";
+
+export default function useGetConcert(concertId) {
+  const [concert, setConcert] = React.useState(null);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [errorMessage, setErrorMessage] = React.useState(false);
+
+  React.useEffect(() => {
+    async function doGetConcert() {
+      try {
+        setIsLoading(true);
+        const concert = await getConcert(concertId);
+        setConcert(concert);
+        setIsLoading(false);
+      } catch (error) {
+        setErrorMessage(error.message);
+      }
+    }
+    doGetConcert();
+  }, [concertId]);
+
+  return { concert, isLoading, errorMessage };
+}
