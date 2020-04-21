3f94136db0335b43688d2db2b5b78d11851de7b5
nadinho <rockt2935@gmail.com>
2020-04-21 10:33:10 +0200

    Add function to get concert by id

--- client/src/api/Concerts.js
+++ client/src/api/Concerts.js
@@ -6,5 +6,14 @@ export async function postConcert(concerts) {
     },
     body: JSON.stringify(concerts),
   });
-  await response.json();
+  const createdConcert = await response.json();
+  return createdConcert;
+}
+
+// User should be redirect to created concert
+
+export async function getConcert(concertId) {
+  const response = await fetch(`http://localhost:4000/concerts/${concertId}`);
+  const concert = await response.json();
+  return concert;
 }
