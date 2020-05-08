faa13621075e53fa1a01044676f8099a93cd8134
nadinho <rockt2935@gmail.com>
2020-05-08 17:08:25 +0200

    Add user models and routes

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
 const concerts = require("./lib/routes/concerts");
+const users = require("./lib/routes/users");
+const auth = require("./lib/routes/auth");
 
 const port = process.env.PORT || 8090;
 const app = express();
 
 // Parse application/json for all request
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/concerts", concerts);
+app.use("/api/auth", auth);
+app.use("/api/users", users);
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
