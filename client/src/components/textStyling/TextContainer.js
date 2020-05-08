f9b62f0aa5e67dd270a730f60ca0b95a46c3361b
nadinho <rockt2935@gmail.com>
2020-05-08 17:15:34 +0200

    Adjust stylings

--- client/src/components/textStyling/TextContainer.js
+++ client/src/components/textStyling/TextContainer.js
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 export const TextContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 80%;
+  text-align: center;
   margin-top: 20px;
+  padding: 20px;
 `;
